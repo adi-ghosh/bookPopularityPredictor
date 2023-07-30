bad99fdfd0b2d5a270a6ffb1dc04a849c602a023
adighosh18 <112495570+adighosh18@users.noreply.github.com>
2023-07-29 20:51:49 -0400

    Update main.py
    Added new models.

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ import pandas as pd
 # import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.corpus import stopwords
-# from sklearn.linear_model import SGDClassifier
-# from sklearn.pipeline import Pipeline
-# from keras.layers import Dropout
-# from keras.models import Sequential
-# from keras.layers import Dense
-# import tensorflow as tf
-from sklearn.model_selection import cross_val_score, train_test_split
-from sklearn import svm
+from nltk import download
+from sklearn.model_selection import train_test_split
+from sklearn import svm, preprocessing, linear_model
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
 
 # from tensorflow import keras
 # from tensorflow.keras import layers, models
@@ -18,10 +15,19 @@ from sklearn import svm
 # from tensorflow.keras.layers import Dense
 
 # Press the green button in the gutter to run the script.
+
+
+# def round_of_rating(numbers):
+#     return list(map(lambda v: (v * 2) / 2, numbers))
+
+
 if __name__ == '__main__':
 
     # You need to run this only once, in order to download the stopwords list
-    # nltk.download('stopwords')
+    try:
+        stop_words_list = stopwords.words('english')
+    except LookupError:
+        nltk.download('stopwords')
 
     # Load the stopwords list
     stop_words_list = stopwords.words('english')
@@ -32,21 +38,24 @@ if __name__ == '__main__':
     percentage = 10
     upperlimit = 0.5+(0.5*(percentage/100))
     lowerlimit = 0.5-(0.5*(percentage/100))
-    noofwords = 10000
+    noofwords = 50000
 
     with open('dataset.csv', newline='', encoding='utf-8-sig') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            y.append(row['rating'])
+            y.append(float(row['rating']))
             with open(row['textpath'], encoding="utf8") as f:
                 contents = f.read()
                 contents = re.sub('[^A-Za-z ]+', '', contents)
                 contents = contents.lower()
                 #contents = contents[int(len(contents) * lowerlimit):int(len(contents) * upperlimit)]
                 words = list(map(str, contents.split()))
-                contents = " ".join(random.sample(words, noofwords))
+                #contents = " ".join(random.sample(words, noofwords))
                 " ".join(contents.split())
                 booktextlist.append(contents)
+
+        lab = preprocessing.LabelEncoder()
+        y = lab.fit_transform(y)
 
         # Instantiate CountVectorizer
         cv = CountVectorizer(stop_words=stop_words_list)
@@ -59,6 +68,7 @@ if __name__ == '__main__':
         # Create a dataframe with words and their respective frequency
         # Each row represents a document starting from document1
         X = pd.DataFrame(data=count_list, columns=word_list)
+        # y = round_of_rating(y)
 
         # Print out the df
         # print(X)
@@ -83,10 +93,20 @@ if __name__ == '__main__':
         # np.mean(predicted == twenty_test.target)
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 0)
+
+        # y_pred = clf.predict(X_test)
+        # print(accuracy_score(y_test,y_pred))
+        # print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+
+
         clf = svm.SVC(kernel='linear', C=1, random_state=42).fit(X_train, y_train)
-        print(clf.score(X_test, y_test))
-        #print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+        print("Using Support Vector Machine, accuracy is", clf.score(X_test, y_test))
 
+        clf = linear_model.LogisticRegression().fit(X_train, y_train)
+        print("Using Logistic Regression Model, accuracy is", clf.score(X_test, y_test))
 
+        clf = GaussianNB().fit(X_train, y_train)
+        print("Using Gaussian Naive Bayes Model, accuracy is", clf.score(X_test, y_test))
 
-
+        clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes = (15,), random_state = 1).fit(X_train, y_train)
+        print("Using Deep Neural Network, accuracy is", clf.score(X_test, y_test))
